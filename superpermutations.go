eae767cdf5c9c09a62ab599cdeeb788185b386cc
Gabriel <gabrielj.harel@gmail.com>
2018-02-16 18:31:10 -0500

    made checking use threads

--- superpermutations.go
+++ superpermutations.go
@@ -4,9 +4,7 @@ import (
 	"fmt"
 	"index/suffixarray"
 	"math"
-	"strings"
 	"sync"
-	"time"
 )
 
 // Find computes a superpermutation of the input string.
@@ -52,82 +50,97 @@ func Find(value string) string {
 
 // Check verifies that the the second argument is a superpermutation of the first.
 func Check(input, superpermutation string) bool {
-	start := time.Now()
-	ps := permutations(input)
+	var index *suffixarray.Index
 
-	fmt.Println("permut", time.Now().Sub(start))
+	length := len(input)
+	ps := make([][]byte, factorial(length))
 
-	// calculating bucket indecies
-	bucketCount := int(math.Min(32.0, float64(len(ps))))
-	buckets := make([][2]int, bucketCount)
-	bucketSize := len(ps) / bucketCount
-	bucketDrop := len(ps) % bucketCount
+	// computing permutations in len(input) goroutines
+	wg := distribute(length, func(current, _ int) {
+		inc := current * factorial(length-1)
+		for i, p := range permutations(splice([]byte(input), current)) {
+			ps[inc+i] = append([]byte{input[current]}, p...)
+		}
+	})
+
+	wg.Add(1)
+	go func() {
+		index = suffixarray.New([]byte(superpermutation))
+		wg.Done()
+	}()
+
+	wg.Wait()
+
+	// checking that all permutations are present in the index
+	status := true
+	distribute(len(ps), func(start, end int) {
+		for _, p := range ps[start:end] {
+			if len(index.Lookup(p, 1)) == 0 {
+				status = false
+			}
+		}
+	}).Wait()
+
+	return status
+}
+
+// recursively generates permutations of input slice
+func permutations(input []byte) [][]byte {
+	if len(input) == 1 {
+		return [][]byte{input}
+	}
+
+	p := [][]byte{}
+	for i, char := range input {
+		for _, s := range permutations(splice(input, i)) {
+			p = append(p, append([]byte{char}, s...))
+		}
+	}
+
+	return p
+}
+
+// divides the values of count into almost equal buckets (max difference of 1)
+// calls callback in goroutine for each bucket with start and end indecies
+func distribute(count int, cb func(int, int)) *sync.WaitGroup {
+	maxGoroutines := 32.00
+
+	bucketCount := int(math.Min(maxGoroutines, float64(count)))
+	bucketSize := count / bucketCount
+	bucketDrop := count % bucketCount
+
+	wg := sync.WaitGroup{}
+	wg.Add(bucketCount)
+
 	for i := 0; i < bucketCount; i++ {
 		start := i*bucketSize + int(math.Min(float64(i), float64(bucketDrop)))
 		end := start + bucketSize - 1
 		if i < bucketDrop {
 			end++
 		}
-		buckets = append(buckets, [2]int{start, end})
-	}
 
-	fmt.Println("bucket", time.Now().Sub(start))
-
-	index := suffixarray.New([]byte(superpermutation))
-
-	fmt.Println("index ", time.Now().Sub(start))
-
-	// checking that all permutations are present in the index
-	status := true
-	wg := sync.WaitGroup{}
-	for _, b := range buckets {
-		indecies := b
-		wg.Add(1)
 		go func() {
-			for _, p := range ps[indecies[0]:indecies[1]] {
-				if len(index.Lookup([]byte(p), 1)) == 0 {
-					status = false
-				}
-			}
+			cb(start, end)
 			wg.Done()
 		}()
 	}
-	wg.Wait()
 
-	fmt.Println("done  ", time.Now().Sub(start))
-
-	return status
+	return &wg
 }
 
+// returns hardcoded factorials up to max int
 func factorial(a int) int {
 	f := []int{1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800, 479001600, 6227020800, 87178291200, 1307674368000, 20922789888000, 355687428096000, 6402373705728000, 121645100408832000, 2432902008176640000}
-
 	if a > len(f) {
 		panic(fmt.Errorf("cannot compute factorial above %d", len(f)))
 	}
-
 	return f[a]
 }
 
-// TODO improve perf
-func permutations(input string) []string {
-	var pps func([]string) []string
-
-	pps = func(input []string) []string {
-		if len(input) == 1 {
-			return input
-		}
-		p := []string{}
-		for i, char := range input {
-			subset := []string{}
-			subset = append(subset, input[:i]...)
-			subset = append(subset, input[i+1:]...)
-			for _, s := range pps(subset) {
-				p = append(p, char+s)
-			}
-		}
-		return p
-	}
-
-	return pps(strings.Split(input, ""))
+// threadsafe splice
+func splice(input []byte, index int) []byte {
+	subset := []byte{}
+	subset = append(subset, input[:index]...)
+	subset = append(subset, input[index+1:]...)
+	return subset
 }
